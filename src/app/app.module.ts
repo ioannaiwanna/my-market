b6d45ae7de7194c172a2d1de74447fc3d1f014fd
ioanna_iwanna <ioannaiwanna12@gmail.com>
2023-05-05 13:50:31 +0300

    removed unused app-header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { AppCartComponent } from './app-cart/app-cart.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +11,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    AppHeaderComponent,
     ProductListComponent,
     AppCartComponent,
     HomeComponent,
