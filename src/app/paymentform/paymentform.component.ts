b70d7a9bbb7f7ed03635a94b0e7b99a187e6c723
ioanna_iwanna <ioannaiwanna12@gmail.com>
2023-11-30 21:02:22 +0200

    fixing css flexbox layout in payment component

--- src/app/paymentform/paymentform.component.ts
+++ src/app/paymentform/paymentform.component.ts
@@ -15,86 +15,97 @@ import { Router } from '@angular/router';
   imports: [ReactiveFormsModule, NgIf, NotificationMsgComponent],
   template: `
     <form [formGroup]="cardForm">
-      <div class="flex flex-col space-y-2 ...">
+      <div class="flex flex-col items-center space-y-2">
         <div class="flex flex-row items-center space-x-2">
-          <label for="cardNumber">Card Number</label>
-          <input
-            class="border border-black"
-            type="text"
-            id="cardNumber"
-            formControlName="cardNumber"
-            required
-          />
-        </div>
-        <div class="flex flex-row items-center space-x-2">
-          <label for="cardName">Card Name</label>
-          <input
-            class="border border-black"
-            type="text"
-            id="cardName"
-            formControlName="cardName"
-            required
-          />
-        </div>
-        <div
-          *ngIf="
-            cardNumber?.invalid && (cardNumber?.dirty || cardNumber?.touched)
-          "
-          class="alert alert-danger"
-        >
-          <div *ngIf="cardNumber?.errors?.['required']">
-            Card number is required.
+          <div class="flex flex-row justify-items-center space-x-2">
+            <label for="cardNumber">Card Number</label>
+            <input
+              class="border border-black"
+              type="text"
+              id="cardNumber"
+              formControlName="cardNumber"
+              required
+            />
           </div>
-        </div>
-        <div
-          *ngIf="cardName?.invalid && (cardName?.dirty || cardName?.touched)"
-          class="alert alert-danger"
-        >
-          <div *ngIf="cardName?.errors?.['required']">
-            Card name is required.
+          <div
+            *ngIf="
+              cardNumber?.invalid && (cardNumber?.dirty || cardNumber?.touched)
+            "
+            class="alert alert-danger"
+          >
+            <div *ngIf="cardNumber?.errors?.['required']">
+              Card number is required.
+            </div>
           </div>
         </div>
         <div class="flex flex-row items-center space-x-2">
-          <label for="expirationDate">Expiration Date</label>
-          <input
-            class="border border-black"
-            type="text"
-            id="expirationDate"
-            formControlName="expirationDate"
-            required
-          />
-        </div>
-        <div class="flex flex-row items-center space-x-2">
-          <label for="cvv">CVV</label>
-          <input
-            class="border border-black"
-            type="text"
-            id="cvv"
-            formControlName="cvv"
-            required
-          />
-        </div>
-        <div
-          *ngIf="
-            expirationDate?.invalid &&
-            (expirationDate?.dirty || expirationDate?.touched)
-          "
-          class="alert alert-danger"
-        >
-          <div *ngIf="expirationDate?.errors?.['required']">
-            Expiration date is required.
+          <div class="flex flex-row items-center space-x-2">
+            <label for="cardName">Card Name</label>
+            <input
+              class="border border-black"
+              type="text"
+              id="cardName"
+              formControlName="cardName"
+              required
+            />
+          </div>
+
+          <div
+            *ngIf="cardName?.invalid && (cardName?.dirty || cardName?.touched)"
+            class="alert alert-danger"
+          >
+            <div *ngIf="cardName?.errors?.['required']">
+              Card name is required.
+            </div>
           </div>
         </div>
-        <div
-          *ngIf="cvv?.invalid && (cvv?.dirty || cvv?.touched)"
-          class="alert alert-danger"
-        >
-          <div *ngIf="cvv?.errors?.['required']">CVV is required.</div>
+        <div class="flex flex-row items-center space-x-2">
+          <div class="flex flex-row items-center space-x-2">
+            <label for="expirationDate">Expiration Date</label>
+            <input
+              class="border border-black"
+              type="text"
+              id="expirationDate"
+              formControlName="expirationDate"
+              required
+            />
+          </div>
+          <div
+            *ngIf="
+              expirationDate?.invalid &&
+              (expirationDate?.dirty || expirationDate?.touched)
+            "
+            class="alert alert-danger"
+          >
+            <div *ngIf="expirationDate?.errors?.['required']">
+              Expiration date is required.
+            </div>
+          </div>
         </div>
+        <div class="flex flex-row items-center space-x-2">
+          <div class="flex flex-row items-center space-x-2">
+            <label for="cvv">CVV</label>
+            <input
+              class="border border-black"
+              type="text"
+              id="cvv"
+              formControlName="cvv"
+              required
+            />
+          </div>
+
+          <div
+            *ngIf="cvv?.invalid && (cvv?.dirty || cvv?.touched)"
+            class="alert alert-danger"
+          >
+            <div *ngIf="cvv?.errors?.['required']">CVV is required.</div>
+          </div>
+        </div>
+
         <div
           class="px-4 py-1 text-sm text-purple-600 border-purple-600 rounded-full border hover:text-white hover:bg-violet-600 hover:border-transparent"
         >
-          <button type="submit" (click)="onSubmit()">Pay</button>
+          <button type="submit" (click)="onSubmit()">Proceed to payment</button>
         </div>
       </div>
     </form>
@@ -138,13 +149,11 @@ export class PaymentformComponent implements OnInit {
   }
 
   onSubmit(): void {
+    console.log('clicked');
     if (this.cardForm.valid) {
       this.route.navigate(['/notification'], {
         queryParams: { message: 'GOOD BYEEEEEEE!' },
       });
-      setTimeout(() => {
-        this.location.back();
-      }, 3000);
     }
   }
 }
